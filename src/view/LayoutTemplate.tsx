465bc58f1dd39caa7c083f93dba66230e6bb3bcc
JennyShih <>
2023-04-24 16:45:13 +0800

    add format config

--- src/view/LayoutTemplate.tsx
+++ src/view/LayoutTemplate.tsx
@@ -1,16 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-/**
- * Main app component
- * @return {JSX.Element} App component
- */
 function App() {
+  const test = () => {
+    console.log('test');
+  };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
